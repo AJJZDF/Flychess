89bcb977c471f8d1a93d0a1efefd98e525a8b1ec
Tedsoul <1634875745@qq.com>
2018-04-03 22:20:57 +0800

    udp---修改

--- src/ReceiveUDP.java
+++ src/ReceiveUDP.java
@@ -2,12 +2,17 @@
 import java.net.DatagramPacket;
 
 import java.net.DatagramSocket;
+import java.util.*;
+import java.util.Queue;
 
 
 public class ReceiveUDP {
 
-    public static void main(String[] args) throws Exception {
-
+    Set<String> recvRoom ;
+    ReceiveUDP(){
+        recvRoom= new HashSet<String>();
+    }
+    void recv()throws Exception{
         int listenPort = 9999;
 
         byte[] buf = new byte[1024];
@@ -29,24 +34,36 @@ public class ReceiveUDP {
 
                     + " from address: " + packet.getSocketAddress();
 
+
             System.out.println(rcvd);
 
+            //如果room 集合中没有这个房间（以ip地址作为唯一标识符）
+            if(recvRoom.add(packet.getSocketAddress().toString())){
+                // Send a response packet to sender
+                //这个packet包含了client的局域网地址，可用于建立tcp连接
+                String backData = "halo,server .including client address for tcp";
 
-            // Send a response packet to sender
-            //这个packet包含了client的局域网地址，可用于建立tcp连接
-            String backData = "halo,server .including client address for tcp";
+                byte[] data = backData.getBytes();
 
-            byte[] data = backData.getBytes();
+                System.out.println("Send " + backData + " to " + packet.getSocketAddress());
 
-            System.out.println("Send " + backData + " to " + packet.getSocketAddress());
+                DatagramPacket backPacket = new DatagramPacket(data, 0,
 
-            DatagramPacket backPacket = new DatagramPacket(data, 0,
+                        data.length, packet.getSocketAddress());
 
-                    data.length, packet.getSocketAddress());
+                responseSocket.send(backPacket);
+            }
 
-            responseSocket.send(backPacket);
+
+
 
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ReceiveUDP receiveUDP=new ReceiveUDP();
+
+        receiveUDP.recv();
 
     }
 }
--- src/UDP_server.java
+++ src/UDP_server.java
@@ -1,13 +1,13 @@
-import java.io.BufferedReader;
-
 import java.io.IOException;
-
-import java.io.InputStreamReader;
 
 import java.net.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.UUID;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static java.lang.Thread.sleep;
 
 public class UDP_server {//send udp  广播udp
     //接收
@@ -16,63 +16,14 @@ public class UDP_server {//send udp  广播udp
     @SuppressWarnings("resource")
     final DatagramSocket detectSocket;
 
-    class sendMsg implements Runnable {
-        @Override
-        public void run() {
-
-            System.out.println("Send thread started.");
-
-            while (true) {
-                try {
-
-                    byte[] buf = new byte[1024];
-
-                    int packetPort = 9999;
-
-                    // Broadcast address
-                    InetAddress hostAddress = InetAddress.getByName("192.168.1.255");
-
-                    //输出调试,控制发送的间隔
-                    BufferedReader stdin = new BufferedReader(
-
-                            new InputStreamReader(System.in));
-
-                    String outMessage = stdin.readLine();
-
-
-                    if (outMessage.equals("bye"))
-
-                        break;
-
-                    buf = outMessage.getBytes();
-
-                    System.out.println("Send " + outMessage + " to " + hostAddress);
-
-
-                    // Send packet to hostAddress:9999, server that listen
-                    // 9999 would reply this packet
-
-                    String bufString="Room Number:   "+ UUID.randomUUID().toString();//uuid 表示房间号，绝对唯一
-                    buf=bufString.getBytes();
-                    DatagramPacket out = new DatagramPacket(buf,
-
-                            buf.length, hostAddress, packetPort);
-
-                    detectSocket.send(out);
-
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-    }
-    class recMsg implements Runnable{
+    //接收线程在等到三个玩家回应UDP广播后结束，三个玩家的地址存在queueClient里
+    class recMsg extends Thread{
         @Override
         public void run() {
             System.out.println("Receive thread started.");
+
+            //一直尝试加锁
+            while(!lock_toCheckPlayerReady.tryLock());
 
             while(true) {
 
@@ -90,7 +41,9 @@ public class UDP_server {//send udp  广播udp
                 String rcvd = "Received from " + packet.getSocketAddress();
 
                 //把响应广播的局域网地址，加入到外部类的队列。形式为"/192.168.1.136:8888"
-                UDP_server.this.queuePush(packet.getSocketAddress().toString());
+                boolean checkSuccess= UDP_server.this.queuePush(packet.getSocketAddress().toString());
+                if(!checkSuccess)break;
+
 
                 System.out.println(rcvd);
                 System.out.println("Data="
@@ -98,39 +51,135 @@ public class UDP_server {//send udp  广播udp
                         + new String(packet.getData(), 0, packet.getLength()) );
             }
 
+            //解锁
+            lock_toCheckPlayerReady.unlock();
         }
     }
+    class broadCastMsg extends Thread {
+        public volatile boolean exit = false;
+
+        public String nowRoomNum=UUID.randomUUID().toString();
+        //广播我是房主，识别附近的机器
+        void broadcastRoomMaster(){
+            try {
+                byte[] buf = new byte[1024];
+
+                int packetPort = 9999;
+
+                // Broadcast address
+                InetAddress hostAddress = InetAddress.getByName("192.168.1.255");
+
+
+                //System.out.println("Send " + outMessage + " to " + hostAddress);
+
+                // Send packet to hostAddress:9999, server that listen
+                // 9999 would reply this packet
+
+                String bufString="Room Number:   "+ nowRoomNum;//uuid 表示房间号，绝对唯一
+                buf=bufString.getBytes();
+                DatagramPacket out = new DatagramPacket(buf,
+
+                        buf.length, hostAddress, packetPort);
+
+                detectSocket.send(out);
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        @Override
+        public void run() {
+            while (!exit) {
+                //每隔一秒广播一次我是房主，以供后续发现房间的人联系
+                broadcastRoomMaster();
+
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void setExit(){
+            exit=true;
+        }
+    }
+    private recMsg _thread_recMsg;
+    private broadCastMsg _thread_broadCastMsg;
+    private Lock lock_toCheckPlayerReady =new ReentrantLock();
+
+    UDP_server() throws SocketException {
+        _thread_recMsg =this.new recMsg();
+        _thread_broadCastMsg=this.new broadCastMsg();
+        detectSocket = new DatagramSocket(8888);
+        queueClient=new LinkedList<String>();
+    }
+
+
 
     /*
-    检查队列是否超过三个人（加上自己一共四人）
+    检查队列是否超过三个人（加上自己一共四人）,加了三个人之后开始return false，以退出recv线程
      */
     boolean queuePush(String address){
 
-        if (queueClient.size()<4){
+        if (queueClient.size()<3){
             queueClient.add(address);
+            if(queueClient.size()==3) {
+                //此时满人了，吧broadCast线程关掉
+                _thread_broadCastMsg.setExit();
+                return false;
+            }
             return true;
         }
         else return false;
     }
 
-    private sendMsg _thread_sendMsg;
-    private recMsg _thread_recMsg;
-    UDP_server() throws SocketException {
-        _thread_sendMsg =this.new sendMsg();
-        _thread_recMsg=this.new recMsg();
-        detectSocket = new DatagramSocket(8888);
-        queueClient=new LinkedList<String>();
+    //线程自动销毁
+    void newThreadRecv(){
+        _thread_recMsg.start();
+    }
+    void newThreadBroadCast(){
+        _thread_broadCastMsg.start();
+    }
+
+    Queue<String> getQueueClient(){
+        //试图枷锁
+        myTryLock:
+        while(true) {
+            while (lock_toCheckPlayerReady.tryLock()) {
+                if (queueClient.size() != 3) {//client队列还没有生成好
+                    lock_toCheckPlayerReady.unlock();
+                    try {
+                        sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    continue;
+                }
+                else break myTryLock;//client 队列生成好了
+            }
+            //尝试枷锁失败，继续尝试
+        }
+        return queueClient;
     }
 
     public static void main(String[] args) throws SocketException {
 
         UDP_server udp_server=new UDP_server();
 
-        // Send packet thread
-        new Thread(udp_server._thread_sendMsg).start();
+        //broadCast thread
+        udp_server.newThreadBroadCast();
 
         // Receive packet thread.
-        new Thread(udp_server._thread_recMsg).start();
+        udp_server.newThreadRecv();
+
+        //
+        System.out.println(udp_server.getQueueClient());
+        //udp_server.getQueueClient();
     }
 
 
