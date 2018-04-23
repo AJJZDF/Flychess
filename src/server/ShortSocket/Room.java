f9b65b02e6b582c5307fd450c467cae658b1f0c3
Tedsoul <1634875745@qq.com>
2018-04-23 18:44:14 +0800

    修改room

--- src/server/ShortSocket/Room.java
+++ src/server/ShortSocket/Room.java
@@ -15,36 +15,44 @@ import java.util.Vector;
  */
 public class Room {
 
-    String roomname;
-
-    //详细各个函数还未从InetAddress改成Roommates类
     Vector<Roommates> roommates;
 
-
-    public String roomName;
+    String roomName;
     public String roomMasterName;                           //房主名字，创建房间的时候绑定，用于判断当前玩家可不可以点开始游戏
 
-    private NetworkRoommateState[] roommateStates;          //在非游戏状态时候，当前每个位置的人物的状态
     private boolean isGaming=false;                         //非开始游戏状态则说明，需要维护roommateStates
+    private NetworkRoommateState[] roommateStates;          //在非游戏状态时候，当前每个位置的人物的状态
+
 
     private static final int ROOM_MASTER=0;//初始化为第一个进入房间的人就是你的master
 
 
 
-    public Room(String roomname)
+    public Room(String roomName)
     {
-        this.roomname = roomname;
+        this.roomName = roomName;
         this.roommates = new Vector<>();
+        roommateStates=new NetworkRoommateState[4];
+        for (int i = 0; i <4 ; i++) {
+            roommateStates[i].setPosId(i);
+        }
     }
-    public Room(String roomname, InetAddress address)
+    public Room(String roomName, InetAddress address,String roomMasterName)
     {
-        this.roomname = roomname;
+        this.roomName = roomName;
         this.roommates = new Vector<>();
-        roommates.add(address);
+        this.roomMasterName=roomMasterName;
+
+        roommates.add(new Roommates(address,roomMasterName));
+
+        roommateStates=new NetworkRoommateState[4];
+        for (int i = 0; i <4 ; i++) {
+            roommateStates[i].setPosId(i);
+        }
     }
-    public void addPlayer(InetAddress address)
+    public void addPlayer(InetAddress address,String name)
     {
-        roommates.add(address);
+        roommates.add(new Roommates(address,name));
     }
     public boolean removePlayer(InetAddress address)
     {
@@ -61,9 +69,9 @@ public class Room {
 
     //当协议进入游戏逻辑后，只要是Game前缀的消息都给他调用这个函数
     public void broadcast(Protocol msg) throws IOException {
-
+        if(isGaming)
         for (int i = 0; i < roommates.size(); i++) {
-            _sendMsgto(roommates[i].getInetAddress(),msg);
+            _sendMsgto(roommates.elementAt(i).getInetAddress(),msg);
         }
 
     }
